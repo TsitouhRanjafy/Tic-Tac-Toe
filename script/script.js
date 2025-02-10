0e53a35fe06fb83aab6b42ec440235d56ea2c476
TsitouhRanjafy <radohery09@gmail.com>
2025-02-10 19:19:11 +0100

    init status

--- script/script.js
+++ script/script.js
@@ -15,7 +15,9 @@ var booleanCases = [
     [false,false,false]
 ]
 var winner = '#'
+var playWithMachine = true;
 let cases = document.getElementsByTagName('td') // tout notre cases
+const iconReload = document.getElementById('icon-reload');
 const containerWinner = document.getElementById('container-winner');
 const main = document.getElementById('main')
 const rondHtml = `<div class="rond" ></div>`
@@ -26,6 +28,11 @@ const croisHtml = `<div class="crois">
 
 
 var togglePlayer = true;
+
+const reload = () => {
+    location.reload();
+}
+
 
 /*-------------- NOTRE FONCTION START------------------ */
 
@@ -74,9 +81,10 @@ const checkWinner = () => {
 
 }
 
+
 /*-------------- NOTRE FONCTION FIN ------------------ */
 
-const mainEvent = () => {
+const multiplayerEvent = () => {
     for (let i=0;i <= cases.length - 1;i++){
         cases[i].addEventListener(('click'),() => {
             
@@ -99,7 +107,11 @@ const mainEvent = () => {
             winner = checkWinner()
             if (winner != '#'){
                 cases = null
-                containerWinner.innerText = winner + " WIN"
+                containerWinner.innerHTML = winner + " WIN" + ` 
+                    <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-reload" id="icon-reload" onclick="reload();">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                    </svg>  
+                `
                 containerWinner.classList.add('show')
                 // main.classList.add('disable')
             }
@@ -107,7 +119,15 @@ const mainEvent = () => {
     }
 }
 
-mainEvent()
+const simpleplayer = () => {
+    
+}
+
+if (playWithMachine){
+
+} else {
+    multiplayerEvent();
+}
 
 
 
